395cdf848ec2bbed9f9b386e9588d77b74414c51
juan carlos <dr.jcsa@outlook.com>
2022-08-06 20:32:42 -0400

    Initial Commit

---
+++ Shared/Git_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ExperimentsApp.swift
+//  Shared
+//
+//  Created by dr.jcsa on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Git_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
